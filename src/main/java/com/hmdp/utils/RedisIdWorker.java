7ad539b58518de9b65092f476252f85ad4552ddb
ARExc <1821735306@qq.com>
2024-07-22 16:41:29 +0800

    增加全局ID处理器

---
+++ src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -0,0 +1,41 @@
+package com.hmdp.utils;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * 全局ID生成器
+ */
+public class RedisIdWorker {
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    private static final long BEGIN_TIMESTAMP  = 1704067200L;
+    private static final int COUNT_BITS = 32;
+
+    /**
+     * 生成全局ID
+     * @param keyPrefix
+     * @return
+     */
+    public long nextId(String keyPrefix){
+        //1.生成时间戳
+        LocalDateTime now = LocalDateTime.now();
+        //toEpochSecond() 获取时间戳 -> 距1970-1-1:00:00的秒数
+        //ZoneOffset.UTC 校准国际标准时间UTC
+        long nowSecond = now.toEpochSecond(ZoneOffset.UTC);
+        long timeStamp = nowSecond - BEGIN_TIMESTAMP;
+
+        //2.生成序列号
+        //2.1.获取当前日期，精确到天
+        String date = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
+        //2.2.自增长
+        long count = stringRedisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + date);
+        //3.拼接并返回
+        return timeStamp << COUNT_BITS | count;
+    }
+}
