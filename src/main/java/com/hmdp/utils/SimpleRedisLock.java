9b447ecfddffe4224d5185ba578f0353deb1ceb9
ARExc <1821735306@qq.com>
2024-07-23 14:37:33 +0800

    解决误删Redis分布式锁的问题

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -2,12 +2,18 @@ package com.hmdp.utils;
 
 import org.springframework.data.redis.core.StringRedisTemplate;
 
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
-public class SimpleRedisLock implements ILock{
-    private String name;
-    private final StringRedisTemplate stringRedisTemplate;
+public class SimpleRedisLock implements ILock {
+    //锁前缀==key前缀
     private static final String KEY_PREFIX = "lock:";
+    //锁名称==key名称
+    private final String name;
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    private static final String ID_PREFIX = UUID.randomUUID().toString() + "-";
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
@@ -16,16 +22,23 @@ public class SimpleRedisLock implements ILock{
 
     @Override
     public boolean tryLock(long timeOutSec) {
-        //获取线程标识
-        String threadName = Thread.currentThread().getName();
+        //获取线程标识作为锁标识
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
         //获取锁
-        Boolean result = stringRedisTemplate.opsForValue().setIfAbsent(KEY_PREFIX + name, threadName, timeOutSec, TimeUnit.SECONDS);
+        Boolean result = stringRedisTemplate.opsForValue().setIfAbsent(KEY_PREFIX + name, threadId, timeOutSec, TimeUnit.SECONDS);
         return Boolean.TRUE.equals(result);
     }
 
     @Override
     public void unlock() {
-        //释放锁
-        stringRedisTemplate.delete(KEY_PREFIX + name);
+        //获取线程标识
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        //获取锁中的标识
+        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+        //判断标识是否一致
+        if (threadId.equals(id)) {
+            //释放锁
+            stringRedisTemplate.delete(KEY_PREFIX + name);
+        }
     }
 }
